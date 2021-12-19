6c2a7bb29381e3fc4b256cc2be9a8daca6ab12e0
meisamalifallahi@gmail.com <meisamalifallahi@gamil.com>
2021-12-19 13:00:57 +0330

    Add SyncCalculator

---
+++ CalculatorEngine/SyncCalculator.cs
@@ -0,0 +1,7 @@
+﻿namespace CalculatorEngine;
+
+public abstract class SyncCalculator<TParam,TResult>:ICalculator<TParam,TResult>  where TParam:IParam<TResult>
+{
+    protected abstract TResult Calc(TParam param);
+    public Task<TResult> Calc(TParam param, CancellationToken cancellationToken) => Task.FromResult( Calc(param));
+}
