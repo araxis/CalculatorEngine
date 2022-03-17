8a16c0ee0fd0c131428384af97acd0de87ac6bcf
meisam alifallahi <meisamalifallahi@hotmail.com>
2022-03-17 13:01:26 +0330

    deperated SyncCalculator  removed.

--- CalculatorEngine/SyncCalculator.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CalculatorEngine;
-
-public abstract class SyncCalculator<TParam,TResult>:ICalculator<TParam,TResult>  where TParam:IParam<TResult>
-{
-    protected abstract TResult Calc(TParam param);
-    public Task<TResult> Calc(TParam param, CancellationToken cancellationToken) => Task.FromResult( Calc(param));
-}
