9776eb979a62ea06f6daa6f3d72eb4d06317b549
meisamalifallahi@gmail.com <meisamalifallahi@gmail.com>
2022-12-09 12:19:20 +0100

    Add install metods

---
+++ CalculatorEngine.Abstract/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CalculatorEngine.Abstract;
+
+public static class Extensions
+{
+
+    public static IServiceCollection InstallCalculators(this IServiceCollection services)
+    {
+
+        InstallCalculators(services, AppDomain.CurrentDomain.GetAssemblies());
+        return services;
+    }
+
+    public static IServiceCollection InstallCalculators(this IServiceCollection services,params Assembly[] assemblies)
+    {
+        if (!assemblies.Any())
+        {
+            throw new ArgumentException("No assemblies found to scan. Supply at least one assembly to scan for calculators.");
+        }
+      
+        services.Scan(s => s.FromAssemblies(assemblies)
+            .AddClasses(c =>
+                c.AssignableTo(typeof(ICalculator<,>)))
+            .AsImplementedInterfaces()
+            .WithTransientLifetime()
+            .AddClasses(c=>c.AssignableTo(typeof(ICalculator<>)))
+            .AsImplementedInterfaces()
+            .WithTransientLifetime());
+        return services;
+    }
+}
